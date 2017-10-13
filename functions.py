725533a6b73ba26669b9517b70fbcecb655da95f
AnanditaGuha <31958631+AnanditaGuha@users.noreply.github.com>
2017-10-13 08:47:39 -0400

    updates

---
+++ functions.py
@@ -0,0 +1,31 @@
+import turtle
+wn = turtle.Screen()
+sachin = turtle.Turtle()
+sachin.color("orange")
+
+andy = turtle.Turtle()
+
+andy = setpos(100,100)
+
+for i in range (4):
+    sachin.forward(100)
+    sachin.left(50)
+    
+for i in range (4):
+    andy.forward(100)
+    andy.left(50)
+
+import turtle
+
+def makesquare(turt):
+    for i in range (4)
+    turt.forward(100)
+    turt.lft(90)
+    
+    
+    
+    mkaesquare(andy)
+    makesquare(sachin)
+    
+    
+        
